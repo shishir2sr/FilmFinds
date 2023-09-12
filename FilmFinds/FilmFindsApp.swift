1531f330a1662341c33b37810c8a0b74150cd74d
Yasir Arefin Tusher <shishir2sr@users.noreply.github.com>
2023-09-12 07:01:38 +0600

    Initial Commit

---
+++ FilmFinds/FilmFindsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmFindsApp.swift
+//  FilmFinds
+//
+//  Created by Yeasir Arefin Tusher on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FilmFindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
